23304a3d2109ece527380e3be855a643dc3fd500
YuliyaZhukova <90321530+YulUltra@users.noreply.github.com>
2023-04-04 14:56:38 +0400

    Hw3 (#8)
    * feat: implement a structire
    
    * feat: add animations for minute- and hour- arrows
    
    * fix: change background to the better one, improve README file
    
    * feat: add gulp building
    
    * feat: added gulp build
    
    * feat: add configuration, setting fonts and vars
    
    * feat: add html of datatable block
    
    * feat: add all icons and add html of aside block
    
    * feat: add styles to the header block
    
    * feat: add styles to the premium block
    
    * feat: added styles in the chart block
    
    * feat: add outer rositioning of blocks
    
    * fix: fix header's styles
    
    * fix: fix premium's styles
    
    * fix: fix sidebar's, datatable styles
    
    * improve: fix styles and add @media
    
    * feat: add scrolable datatable
    
    * fix: add @media and fix styles
    
    * add build
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * feat: put node_modules in gitignore file
    
    * fix: removed node_modules
    
    * fix: fix fonts link into index.html
    
    * fix: format documents
    
    * fix: format documents
    
    * add DS_Store to gitignore
    
    * fix: deletes sidebar_inner class from html
    
    * fix: capitalize letters in sidebar's title
    
    * renamed app to src folder
    
    * fix: changed paths into gulpfile
    
    * change settings of gulpfile.js
    
    ---------
    
    Co-authored-by: Yuliya Zhukova <yulultra@Yuliyas-MacBook-Pro.local>

---
+++ hw3_bem_graphics_design/clock_animation/README.md
@@ -0,0 +1,3 @@
+Original URL: [https://codepen.io/varvara_ing1/pen/dyqvKQP](https://codepen.io/varvara_ing1/pen/dyqvKQP).
+
+Task: to implement an animation "like at the .gif-example", using only CSS/HTML, "time" can be not clear, but the minute arrow must be slower, than the other one.
---
+++ hw3_bem_graphics_design/responsive_layout/gulpfile.js
@@ -0,0 +1,83 @@
+const { src, dest, watch, parallel, series } = require("gulp")
+
+const scss = require("gulp-sass")(require("sass"))
+const concat = require("gulp-concat")
+const uglify = require("gulp-uglify-es").default
+const browserSync = require("browser-sync")
+const autoprefixer = require("gulp-autoprefixer")
+const clean = require("gulp-clean")
+const imagemin = require("gulp-imagemin")
+
+function scripts() {
+  return src(["dist/js/main.js"])
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("dist/js"))
+    .pipe(browserSync.stream())
+}
+
+function styles() {
+  return src("dist/scss/style.scss")
+    .pipe(
+      autoprefixer({ overrideBrowserslist: ["last 10 version"], grid: true })
+    )
+    .pipe(concat("style.min.css"))
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(dest("dist/css"))
+    .pipe(browserSync.stream())
+}
+
+function watching() {
+  watch(["src/scss/style.scss"], styles)
+  watch(["src/js/main.js"], scripts)
+  watch(["src/*.html"]).on("change", browserSync.reload)
+}
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: "src/",
+    },
+  })
+}
+
+function building() {
+  return src(
+    [
+      "src/css/style.min.css",
+      "src/js/main.min.js",
+      "src/**/*.html",
+    ],
+    {
+      base: "src",
+    }
+  ).pipe(dest("dist"))
+}
+
+function cleanDist() {
+  return src("dist").pipe(clean())
+}
+
+function images() {
+  return src("src/images/**/*")
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(dest("dist/images"))
+}
+
+exports.styles = styles
+exports.scripts = scripts
+exports.watching = watching
+exports.browsersync = browsersync
+exports.images = images
+
+exports.build = series(cleanDist, images, building)
+exports.default = parallel(styles, scripts, browsersync, watching)
