6952c54b8d0cd2065aecfa21fac979c17253899d
YuliyaZhukova <90321530+YulUltra@users.noreply.github.com>
2023-05-06 14:53:44 +0400

    Update hw5-10_to_do_list/react_ts/src/repositories/Repository.ts
    Co-authored-by: Dmitry Krupenya <70685561+dkrupenya-quantori@users.noreply.github.com>

--- hw5-10_to_do_list/react_ts/src/repositories/Repository.ts
+++ hw5-10_to_do_list/react_ts/src/repositories/Repository.ts
@@ -2,7 +2,7 @@ export type EntityID = number
 
 export interface Repository<T> {
     getAll(): Array<T> | Promise<Array<T>>
-    add(entity: T): T | Promise<T>
+    add(entity: T): Promise<T>
     update(entity: T): void | Promise<void>
     deleteById(entityId: EntityID): void | Promise<void>
 }
