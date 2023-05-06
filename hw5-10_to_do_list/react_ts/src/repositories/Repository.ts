5e9217dfafa1b56b6c62db4c01a2b0773d9d4e58
YuliyaZhukova <90321530+YulUltra@users.noreply.github.com>
2023-05-06 23:19:47 +0400

    Revert "To Do List implementation with React (#13)"
    This reverts commit f40d7820f266fea2f7a21701d1805791e8774d83.

--- hw5-10_to_do_list/react_ts/src/repositories/Repository.ts
+++
@@ -1,9 +0,0 @@
-export type EntityID = number
-
-export interface Repository<T> {
-    getAll(): Array<T> | Promise<Array<T>>
-    add(entity: T): Promise<T>
-    update(entity: T): void | Promise<void>
-    deleteById(entityId: EntityID): void | Promise<void>
-}
-
--- hw5-10_to_do_list/react_ts/src/types/Task.ts
+++
@@ -1,9 +0,0 @@
-import { EntityID } from "../repositories/Repository";
-
-export default interface Task {
-    id?: EntityID;
-    name: string;
-    label: string;
-    date: string;
-    isCompleted: boolean;
-}
